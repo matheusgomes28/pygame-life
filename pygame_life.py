1b35a5f58ba8e2bebc30e7d7183b74f40f13ce21
Matheus Gomes <mgomes1@jaguarlandrover.com>
2021-08-03 18:29:26 +0100

    Getting rid of some functions in favour of inlining, more simplifying.

--- pygame_life.py
+++ pygame_life.py
@@ -1,19 +1,44 @@
-import pygame
 import sys
 import time
 
+from collections import defaultdict
 from copy import deepcopy
 from example_grids import gosper_glider
+import pygame
 
-def init_graphics(width, height):
 
-    pygame.init()
-    screen = pygame.display.set_mode((width, height))
-    return screen
+def get_alive_neighbours(grid, x, y):
+    adjacent_dirs = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+    positions = [(x + xAdd, y + yAdd) for xAdd, yAdd in adjacent_dirs]
+    return {(pos[0], pos[1]) for pos in positions if pos in grid.cells}
+
+
+def get_dead_neighbours(grid, x, y):
+    adjacent_dirs = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+    positions = [(x + xAdd, y + yAdd) for xAdd, yAdd in adjacent_dirs]
+    return {(pos[0], pos[1]) for pos in positions if pos not in grid.cells}
+
+
+def update_grid(grid):
+    new_grid = deepcopy(grid)
+    undead = defaultdict(int)
+
+    for (x, y) in grid.cells:
+        alive_neighbours = get_alive_neighbours(grid, x, y)
+        if len(alive_neighbours) < 2 or len(alive_neighbours) > 3:
+            new_grid.cells.remove((x, y))
+
+        for pos in get_dead_neighbours(grid, x, y):
+            undead[pos] += 1
+
+    for pos, neighbours in undead.items():
+        if neighbours == 3:
+            new_grid.cells.add((pos[0], pos[1]))
+
+    return new_grid
 
 
 def draw_grid(screen, grid):
-
     cell_width = screen.get_width()/grid.dim.width
     cell_height = screen.get_height()/grid.dim.height
 
@@ -22,52 +47,16 @@ def draw_grid(screen, grid):
         pygame.draw.rect(screen, (255, 0, 0), rect)
 
 
-def get_alive_neighbours(grid, x, y):
-
-    dirs = [(-1, -1), (0, -1), (1, -1), (-1,  0), (1,  0), (-1, 1), (0, 1), (1, 1)]
-    positions = [(x + xAdd, y + yAdd) for xAdd, yAdd in dirs]
-    return set([(pos[0], pos[1]) for pos in positions if pos in grid.cells])
-
-
-def get_dead_neighbours(grid, x, y):
-
-    dirs = [(-1, -1), (0, -1), (1, -1), (-1,  0), (1,  0), (-1, 1), (0, 1), (1, 1)]
-    positions = [(x + xAdd, y + yAdd) for xAdd, yAdd in dirs]
-    return set([(pos[0], pos[1]) for pos in positions if pos not in grid.cells])
-
-
-def update_grid(grid):
-    new_grid = deepcopy(grid)
-    undead = {}
-
-    for (x, y) in grid.cells:
-        alive_neighbours = get_alive_neighbours(grid, x, y)
-        if len(alive_neighbours) < 2 or len(alive_neighbours) > 3:
-            new_grid.cells.remove((x, y))
-
-        for pos in get_dead_neighbours(grid, x, y):
-            if pos in undead.keys():
-                undead[pos] += 1
-            else:
-                undead[pos] = 1
-
-    for k, v in undead.items():
-        if v == 3:
-            new_grid.cells.add((k[0], k[1]))
-
-    return new_grid
-
-
-if __name__ == "__main__":
+def main():
 
     grid = gosper_glider
-    screen = init_graphics(600, 400)
-    print(grid)
+
+    pygame.init()
+    screen = pygame.display.set_mode((600, 400))
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit(0)
+        if pygame.QUIT in [e.type for e in pygame.event.get()]:
+            sys.exit(0)
 
         screen.fill((0, 0, 0))
 
@@ -76,3 +65,7 @@ if __name__ == "__main__":
         pygame.display.flip()
 
         time.sleep(0.2)
+
+
+if __name__ == "__main__":
+    main()
