0ebbc1c4ab700caa35fb4f46b5a7cf1ecd20d80a
Matheus Gomes <mgomes1@jaguarlandrover.com>
2021-09-02 16:55:35 +0100

    Quality improvements and strong types.

--- grid_defs.py
+++ grid_defs.py
@@ -2,3 +2,4 @@ from collections import namedtuple
 
 Dim = namedtuple("Dimension", ["width", "height"])
 Grid = namedtuple("Grid", ["dim", "cells"])
+Neighbours = namedtuple("Neighbours", ["alive", "dead"])
--- pygame_life.py
+++ pygame_life.py
@@ -6,31 +6,28 @@ from copy import deepcopy
 import pygame
 
 from example_grids import gosper_glider
-from grid_defs import Grid
-
-adjacent_dirs = [(-1, -1), (0, -1), (1, -1), (-1, 0),
-                 (1, 0), (-1, 1), (0, 1), (1, 1)]
+from grid_defs import Grid, Neighbours
 
 
-def get_alive_neighbours(grid, x, y):
-    positions = [(x + xAdd, y + yAdd) for xAdd, yAdd in adjacent_dirs]
-    return {(pos[0], pos[1]) for pos in positions if pos in grid.cells}
+def get_neighbours(grid: Grid, x: int, y: int) -> Neighbours:
+    offsets = [(-1, -1), (0, -1), (1, -1), (-1, 0),
+               (1, 0), (-1, 1), (0, 1), (1, 1)]
+    possible_neighbours = {(x + x_add, y + y_add) for x_add, y_add in offsets}
+    alive = {(pos[0], pos[1])
+             for pos in possible_neighbours if pos in grid.cells}
+    return Neighbours(alive, possible_neighbours - alive)
 
 
-def get_dead_neighbours(grid, x, y):
-    positions = [(x + xAdd, y + yAdd) for xAdd, yAdd in adjacent_dirs]
-    return {(pos[0], pos[1]) for pos in positions if pos not in grid.cells}
-
-
-def update_grid(grid):
+def update_grid(grid: Grid) -> Grid:
     new_cells = deepcopy(grid.cells)
     undead = defaultdict(int)
 
     for (x, y) in grid.cells:
-        if len(get_alive_neighbours(grid, x, y)) not in [2, 3]:
+        alive_neighbours, dead_neighbours = get_neighbours(grid, x, y)
+        if len(alive_neighbours) not in [2, 3]:
             new_cells.remove((x, y))
 
-        for pos in get_dead_neighbours(grid, x, y):
+        for pos in dead_neighbours:
             undead[pos] += 1
 
     for pos, _ in filter(lambda elem: elem[1] == 3, undead.items()):
@@ -39,18 +36,17 @@ def update_grid(grid):
     return Grid(grid.dim, new_cells)
 
 
-def draw_grid(screen, grid):
-    cell_width = screen.get_width()/grid.dim.width
-    cell_height = screen.get_height()/grid.dim.height
+def draw_grid(screen: pygame.Surface, grid: Grid) -> None:
+    cell_width = screen.get_width() / grid.dim.width
+    cell_height = screen.get_height() / grid.dim.height
+    border_size = 2
 
     for (x, y) in grid.cells:
-        rect = pygame.Rect(x*cell_width+2, y*cell_height +
-                           2, cell_width-2, cell_height-2)
-        pygame.draw.rect(screen, (255, 0, 0), rect)
+        pygame.draw.rect(screen, (255, 0, 0), (x * cell_width + border_size, y * cell_height +
+                                               border_size, cell_width - border_size, cell_height - border_size))
 
 
 def main():
-
     grid = gosper_glider
 
     pygame.init()
